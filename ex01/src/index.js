8b24f79653403012ea057ef924fa08bc93ee05bf
neven kusturica <neven.kusturica@gmail.com>
2022-04-18 00:24:34 +0200

    Added ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,241 @@
+
+let display = document.getElementById("display");
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btn0 = document.getElementById("btn0");
+let btnSp = document.getElementById("btnSp");
+let btnQ = document.getElementById("btnQ");
+let btnW = document.getElementById("btnW");
+let btnE = document.getElementById("btnE");
+let btnR = document.getElementById("btnR");
+let btnT = document.getElementById("btnT");
+let btnY = document.getElementById("btnY");
+let btnU = document.getElementById("btnU");
+let btnI = document.getElementById("btnI");
+let btnO = document.getElementById("btnO");
+let btnP = document.getElementById("btnP");
+let btnA = document.getElementById("btnA");
+let btnS = document.getElementById("btnS");
+let btnD = document.getElementById("btnD");
+let btnF = document.getElementById("btnF");
+let btnG = document.getElementById("btnG");
+let btnH = document.getElementById("btnH");
+let btnJ = document.getElementById("btnJ");
+let btnBs = document.getElementById("btnBs");
+
+
+
+
+
+let temoTxt = "";
+
+const btn1F = () => {
+    display.innerHTML = temoTxt + "1";
+    temoTxt = temoTxt + "1";
+}
+btn1.onclick = function () {
+    btn1F();
+};
+const btn2F = () => {
+    display.innerHTML = temoTxt + "2";
+    temoTxt = temoTxt + "2";
+}
+btn2.onclick = function () {
+    btn2F();
+};
+const btn3F = () => {
+    display.innerHTML = temoTxt + "3";
+    temoTxt = temoTxt + "3";
+}
+btn3.onclick = function () {
+    btn3F();
+};
+const btn4F = () => {
+    display.innerHTML = temoTxt + "4";
+    temoTxt = temoTxt + "4";
+}
+btn4.onclick = function () {
+    btn4F();
+};
+const btn5F = () => {
+    display.innerHTML = temoTxt + "5";
+    temoTxt = temoTxt + "5";
+}
+btn5.onclick = function () {
+    btn5F();
+};
+const btn6F = () => {
+    display.innerHTML = temoTxt + "6";
+    temoTxt = temoTxt + "6";
+}
+btn6.onclick = function () {
+    btn6F();
+};
+const btn7F = () => {
+    display.innerHTML = temoTxt + "7";
+    temoTxt = temoTxt + "7";
+}
+btn7.onclick = function () {
+    btn7F();
+};
+const btn8F = () => {
+    display.innerHTML = temoTxt + "8";
+    temoTxt = temoTxt + "8";
+}
+btn8.onclick = function () {
+    btn8F();
+};
+const btn9F = () => {
+    display.innerHTML = temoTxt + "9";
+    temoTxt = temoTxt + "9";
+}
+btn9.onclick = function () {
+    btn9F();
+};
+const btn0F = () => {
+    display.innerHTML = temoTxt + "0";
+    temoTxt = temoTxt + "0";
+}
+btn0.onclick = function () {
+    btn0F();
+};
+
+const btnSpf = () => {
+    display.innerHTML = temoTxt + " ";
+    temoTxt = temoTxt + " ";
+}
+btnSp.onclick = function () {
+    btnSpf();
+}
+
+const btnQf = () => {
+    display.innerHTML = temoTxt + "Q";
+    temoTxt = temoTxt + "Q";
+}
+btnQ.onclick = function () {
+    btnQf();
+}
+
+const btnWf = () => {
+    display.innerHTML = temoTxt + "W";
+    temoTxt = temoTxt + "W";
+}
+btnW.onclick = function () {
+    btnWf();
+}
+const btnEf = () => {
+    display.innerHTML = temoTxt + "E";
+    temoTxt = temoTxt + "E";
+}
+btnE.onclick = function () {
+    btnEf();
+}
+const btnRf = () => {
+    display.innerHTML = temoTxt + "R";
+    temoTxt = temoTxt + "R";
+}
+btnR.onclick = function () {
+    btnRf();
+}
+const btnTf = () => {
+    display.innerHTML = temoTxt + "T";
+    temoTxt = temoTxt + "T";
+}
+btnT.onclick = function () {
+    btnTf();
+}
+const btnYf = () => {
+    display.innerHTML = temoTxt + "Y";
+    temoTxt = temoTxt + "Y";
+}
+btnY.onclick = function () {
+    btnYf();
+}
+const btnUf = () => {
+    display.innerHTML = temoTxt + "U";
+    temoTxt = temoTxt + "U";
+}
+btnU.onclick = function () {
+    btnUf();
+}
+const btnIf = () => {
+    display.innerHTML = temoTxt + "I";
+    temoTxt = temoTxt + "I";
+}
+btnI.onclick = function () {
+    btnIf();
+}
+
+const btnOf = () => {
+    display.innerHTML = temoTxt + "O";
+    temoTxt = temoTxt + "O";
+}
+btnO.onclick = function () {
+    btnOf();
+}
+const btnPf = () => {
+    display.innerHTML = temoTxt + "P";
+    temoTxt = temoTxt + "P";
+}
+btnP.onclick = function () {
+    btnPf();
+}
+const btnAf = () => {
+    display.innerHTML = temoTxt + "A";
+    temoTxt = temoTxt + "A";
+}
+btnA.onclick = function () {
+    btnAf();
+}
+const btnSf = () => {
+    display.innerHTML = temoTxt + "S";
+    temoTxt = temoTxt + "S";
+}
+btnS.onclick = function () {
+    btnSf();
+}
+const btnDf = () => {
+    display.innerHTML = temoTxt + "D";
+    temoTxt = temoTxt + "D";
+}
+btnD.onclick = function () {
+    btnDf();
+}
+const btnFf = () => {
+    display.innerHTML = temoTxt + "F";
+    temoTxt = temoTxt + "F";
+}
+btnF.onclick = function () {
+    btnFf();
+}
+const btnGf = () => {
+    display.innerHTML = temoTxt + "G";
+    temoTxt = temoTxt + "G";
+}
+btnG.onclick = function () {
+    btnGf();
+}
+const btnHf = () => {
+    display.innerHTML = temoTxt + "H";
+    temoTxt = temoTxt + "H";
+}
+btnH.onclick = function () {
+    btnHf();
+}
+const btnJf = () => {
+    display.innerHTML = temoTxt + "J";
+    temoTxt = temoTxt + "J";
+}
+btnJ.onclick = function () {
+    btnJf();
+}
+
+
